e53cded552ff4e47f3d1be70156e66eb207f02ae
Vinicius Araujo <vinidev.morais@gmail.com>
2026-05-27 17:16:33 -0300

    feat: add support for install offline

--- src/Interfaces/IFeatureInstaller.cs
+++ src/Interfaces/IFeatureInstaller.cs
@@ -5,5 +5,5 @@ namespace InstaladorNewAcesso.Interfaces;
 public interface IFeatureInstaller
 {
     Task<bool> IsFeatureInstalledAsync(WindowsFeature feature);
-    Task<bool> InstallFeatureAsync(WindowsFeature feature);
+    Task<bool> InstallFeatureAsync(WindowsFeature feature, string? sxsPath = null);
 }
