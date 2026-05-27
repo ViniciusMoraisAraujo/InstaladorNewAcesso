3107fd0494327f3cdc796f13d59faf71a257815d
Vinicius Araujo <vinidev.morais@gmail.com>
2026-05-27 12:52:14 -0300

    feat: implement server intaller

---
+++ src/Implementations/WindowsServerInstaller.cs
@@ -0,0 +1,14 @@
+﻿using InstaladorNewAcesso.Interfaces;
+using InstaladorNewAcesso.Models;
+using InstaladorNewAcesso.Utils;
+
+namespace InstaladorNewAcesso.Implementations;
+
+public class WindowsServerInstaller : IFeatureInstaller
+{
+    public async Task<bool> InstallFeatureAsync(WindowsFeature feature)
+    {
+        string arguments = $"-Command \"Install-WindowsFeature -Name {feature.ServerName} -IncludeAllSubFeature\"";
+        return await ProcessExecutor.RunPowerShellCommandAsync(arguments, feature.FriendlyName);
+    }
+}
