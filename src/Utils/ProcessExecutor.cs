04e461b138aa0dd7a0dcda380d5db9464e08d999
Vinicius Araujo <vinidev.morais@gmail.com>
2026-05-27 13:22:34 -0300

    feat: implement executor in powershell

---
+++ src/Utils/ProcessExecutor.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace InstaladorNewAcesso.Utils;
+
+public static class ProcessExecutor
+{
+    public static async Task<bool> RunPowerShellCommandAsync(string arguments, string featureName)
+    {
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = "powershell.exe",
+            Arguments = arguments,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            Verb = "runas"
+        };
+
+        try
+        {
+            using var process = new Process { StartInfo = startInfo };
+            process.Start();
+
+            var output = await process.StandardOutput.ReadToEndAsync();
+            var error = await process.StandardError.ReadToEndAsync();
+
+            await process.WaitForExitAsync();
+
+            if (process.ExitCode == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[SUCESSO] Ativado: {featureName}");
+                return true;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[FAIL] Ativado: {featureName}");
+            return false;
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[FAIL] Erro: {featureName}");
+            return false;
+        }
+        finally
+        {
+            Console.ResetColor();
+        }
+    }
+}
