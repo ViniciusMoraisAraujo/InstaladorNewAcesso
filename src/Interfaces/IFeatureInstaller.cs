232dab285ec6e69ed9f722d286abe6915a2fb6b1
Vinicius Araujo <vinidev.morais@gmail.com>
2026-05-27 12:52:57 -0300

    feat: create interface por installer

---
+++ src/Interfaces/IFeatureInstaller.cs
@@ -0,0 +1,8 @@
+﻿using InstaladorNewAcesso.Models;
+
+namespace InstaladorNewAcesso.Interfaces;
+
+public interface IFeatureInstaller
+{
+    Task<bool> InstallFeatureAsync(WindowsFeature feature);
+}
