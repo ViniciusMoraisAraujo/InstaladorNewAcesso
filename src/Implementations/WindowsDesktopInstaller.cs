71e90357656154a849ed33c45e83661fb042daa6
Vinicius Araujo <vinidev.morais@gmail.com>
2026-05-27 14:01:17 -0300

    feat: Implement feature installation status check

--- src/Implementations/WindowsDesktopInstaller.cs
+++ src/Implementations/WindowsDesktopInstaller.cs
@@ -6,9 +6,17 @@ namespace InstaladorNewAcesso.Implementations;
 
 public class WindowsDesktopInstaller : IFeatureInstaller
 {
+    public async Task<bool> IsFeatureInstalledAsync(WindowsFeature feature)
+    {
+        var arguments = $"-Command \"(Get-WindowsOptionalFeature -Online -FeatureName {feature.DesktopName}).State\"";
+        var output = await ProcessExecutor.RunPowerShellWithOutputAsync(arguments);
+        
+        return output.Trim().Equals("Enabled", StringComparison.OrdinalIgnoreCase);
+    }
+
     public async Task<bool> InstallFeatureAsync(WindowsFeature feature)
     {
-        string arguments = $"-Command \"Enable-WindowsOptionalFeature -Online -FeatureName {feature.DesktopName} -All -NoRestart\"";
+        var arguments = $"-Command \"Enable-WindowsOptionalFeature -Online -FeatureName {feature.DesktopName} -All -NoRestart\"";
         return await ProcessExecutor.RunPowerShellCommandAsync(arguments, feature.FriendlyName);
     }
 }
--- src/Implementations/WindowsServerInstaller.cs
+++ src/Implementations/WindowsServerInstaller.cs
@@ -6,9 +6,17 @@ namespace InstaladorNewAcesso.Implementations;
 
 public class WindowsServerInstaller : IFeatureInstaller
 {
+    public async Task<bool> IsFeatureInstalledAsync(WindowsFeature feature)
+    {
+        var arguments = $"-Command \"(Get-WindowsFeature -Name {feature.ServerName}).Installed\"";
+        var output = await ProcessExecutor.RunPowerShellWithOutputAsync(arguments);
+        
+        return output.Trim().Equals("True", StringComparison.OrdinalIgnoreCase);
+    }
+
     public async Task<bool> InstallFeatureAsync(WindowsFeature feature)
     {
-        string arguments = $"-Command \"Install-WindowsFeature -Name {feature.ServerName} -IncludeAllSubFeature\"";
+        var arguments = $"-Command \"Install-WindowsFeature -Name {feature.ServerName} -IncludeAllSubFeature\"";
         return await ProcessExecutor.RunPowerShellCommandAsync(arguments, feature.FriendlyName);
     }
 }
