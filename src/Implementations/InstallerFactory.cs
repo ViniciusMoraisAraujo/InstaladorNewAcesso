015be6de05dfabaa7b494c5465a3989e9ecad745
Vinicius Araujo <vinidev.morais@gmail.com>
2026-05-27 13:23:05 -0300

    feat: implement factory pattern for install features

---
+++ src/Implementations/InstallerFactory.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+using InstaladorNewAcesso.Interfaces;
+using Microsoft.Win32;
+
+namespace InstaladorNewAcesso.Implementations;
+
+public static class InstallerFactory
+{
+    public static IFeatureInstaller Create()
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            throw new PlatformNotSupportedException("Este programa roda apenas no Windows");
+        }
+
+        if (IsWindowsServer())
+        {
+            Console.WriteLine("AMBIENTE: WINDOWS SERVER (ServerManage)");
+            return new WindowsServerInstaller();
+        }
+
+        Console.WriteLine("AMBIENTE: WINDOWS DESKTOP (DISM)");
+        return new WindowsDesktopInstaller();
+    }
+
+    private static bool IsWindowsServer()
+    {
+        try
+        {
+            using var key = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows NT\CurrentVersion");
+            var installationType = key.GetValue("InstallationType")?.ToString();
+            return installationType != null && installationType.Contains("Server", StringComparison.OrdinalIgnoreCase);
+        }
+        catch 
+        {
+            return RuntimeInformation.OSDescription.Contains("Server", StringComparison.OrdinalIgnoreCase);        
+        }
+    }
+}
