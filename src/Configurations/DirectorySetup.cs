bb39b418f0aac16090d34a805ae5837927821e96
Vinicius Araujo <vinidev.morais@gmail.com>
2026-05-28 13:59:20 -0300

    feat: create setup for diretories

---
+++ src/Configurations/DirectorySetup.cs
@@ -0,0 +1,25 @@
+﻿using InstaladorNewAcesso.Models;
+
+namespace InstaladorNewAcesso.Configurations;
+
+public class DirectorySetup
+{
+    private static readonly Dictionary<string, string[]> FoldersWithChildren = new()
+    {
+        ["Controller"] = ["ControleAcesso", "CoreWs", "Fabricantes", "Task"],
+        ["ControllerOffline"] = ["Arquivos", "WinService_Ex", "WinService_In"]
+    };
+
+    public IEnumerable<string> GetAllPaths(InstallationPaths paths)
+    {
+        foreach (var folder in paths.GetBaseFolders())
+            yield return folder;
+
+        foreach (var (parent, children) in FoldersWithChildren)
+        {
+            yield return Path.Combine(paths.NewAcessoRoot, parent);
+            foreach (var child in children)
+                yield return Path.Combine(paths.NewAcessoRoot, parent, child);
+        }
+    }
+}
