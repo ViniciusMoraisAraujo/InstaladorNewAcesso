3636a06dee3cddb0dbad2b56f87a40eb27e375a9
Vinicius Araujo <vinidev.morais@gmail.com>
2026-05-28 14:02:24 -0300

    feat: create model for paths

---
+++ src/Models/InstallationPaths.cs
@@ -0,0 +1,31 @@
+﻿namespace InstaladorNewAcesso.Models;
+
+public class InstallationPaths
+{
+    public string BasePath { get; private set; }
+    public string InstallationPath => Path.Combine(BasePath, "Instaladores");
+    public string NewAcessoRoot => Path.Combine(BasePath, "NewAcesso");
+    public string AutoAtendimento => Path.Combine(NewAcessoRoot, "AutoAtendimento");
+    public string ConnectionRecord => Path.Combine(NewAcessoRoot, "ConnectionRecord");
+    public string Controller => Path.Combine(NewAcessoRoot, "Controller");
+    public string ControllerOffline => Path.Combine(NewAcessoRoot, "ControllerOffline");
+    public string VisitAuthorization => Path.Combine(NewAcessoRoot, "VisitAuthorization");
+    public string WebAppDS => Path.Combine(NewAcessoRoot, "WebAppDS");
+    public string WebAppUI => Path.Combine(NewAcessoRoot, "WebAppUI");
+    public string Win => Path.Combine(NewAcessoRoot, "Win");
+
+    public InstallationPaths(string basePath)
+    {
+        BasePath = basePath;
+    }
+   
+    public IEnumerable<string> GetBaseFolders() =>
+    [
+        AutoAtendimento,
+        ConnectionRecord,
+        VisitAuthorization,
+        WebAppDS,
+        WebAppUI,
+        Win
+    ];
+}
