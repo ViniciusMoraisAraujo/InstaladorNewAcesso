12df7ffb1bcbe2d8939d6c3fde519a3648c5961d
Vinicius Araujo <vinidev.morais@gmail.com>
2026-05-28 17:40:58 -0300

    feat: create msi models

---
+++ src/Models/MsiInstallationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace InstaladorNewAcesso.Models;
+
+public class MsiInstallationModel
+{
+    public string MsiPath { get; set; } = "";
+    public string TargetDirectory { get; set; } = "";
+    public bool IsWebApp { get; set; }
+    public string? SiteName { get; set; }
+    public string? AppPoolName { get; set; }
+    public string? ExtraArgs { get; set; }
+}
