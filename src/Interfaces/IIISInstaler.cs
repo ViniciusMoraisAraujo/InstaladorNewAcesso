d61be0213d6234dee334ba81d45729c56058b019
Vinicius Araujo <vinidev.morais@gmail.com>
2026-05-28 14:00:44 -0300

    feat: add interface for config iis

---
+++ src/Interfaces/IIISInstaler.cs
@@ -0,0 +1,9 @@
+﻿namespace InstaladorNewAcesso.Interfaces;
+
+public interface IIISInstaler
+{
+    Task<bool> CreateApplicationPoolAsync(string name, string runtimeVersion, string pipelineMode);
+    Task<bool> CreateSiteAsync(string name, string poolName, string physicalPath, int port);
+    Task<bool> SiteExistsAsync(string name);
+    Task<bool> AppPoolExistsAsync(string name);
+}
