a55be7fe0b68396f07d0d06f5024dd12624ad545
Vinicius Araujo <vinidev.morais@gmail.com>
2026-05-29 15:23:28 -0300

    feat: add ISimpleLogger interface for improved logging functionality

---
+++ src/Interfaces/ISimpleLogger.cs
@@ -0,0 +1,9 @@
+﻿namespace InstaladorNewAcesso.Interfaces;
+
+public interface ISimpleLogger
+{
+    void LogInfo(string message);
+    void LogError(string message);
+    void LogWarning(string message);
+    void LogSuccess(string message);
+}
